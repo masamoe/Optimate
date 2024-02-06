61ae2a6505e54bcb36d329b9da977b25f50ff07f
masamoe <105935014+masamoe@users.noreply.github.com>
2024-02-05 16:11:59 -0800

    stating grid layout functionality

--- app/src/main/java/com/example/optimate/businessOwner/BusinessLanding.kt
+++ app/src/main/java/com/example/optimate/businessOwner/BusinessLanding.kt
@@ -2,26 +2,34 @@ package com.example.optimate.businessOwner
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.View
+import android.widget.GridLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.optimate.R
 import com.example.optimate.loginAndRegister.GlobalUserData
 
+
 class BusinessLanding : AppCompatActivity(){
-    private lateinit var titlesBtn: ImageView
     private lateinit var businessName: String
-    private lateinit var title: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_business_landing)
         businessName = GlobalUserData.name
-        title = findViewById(R.id.titles)
-        title.text = businessName
-        titlesBtn = findViewById(R.id.titlesIcon)
-        titlesBtn.setOnClickListener {
-            // Go to the Titles activity
+
+        val layout: GridLayout = findViewById<View>(R.id.choice_grid) as GridLayout
+        val titleButton = LayoutButton(this, com.example.optimate.R.id.layoutBtn)
+        titleButton.setID(R.id.layoutBtn,1)
+        titleButton.setText(R.id.buttonTxt, "Change")
+        titleButton.setImageResource(R.id.buttonImg, R.drawable.ic_roles_foreground)
+        titleButton.setOnClickListener {
             startActivity(Intent(this, TitlesActivity::class.java))
         }
+
+        layout.addView(titleButton)
+// 'childView' can be any View object
+
+
+
+
     }
 }
