e5e5645f3e2e4633a3b95859b3cc17a007759cb1
ebrahimovic <121978241+ebrahimovic@users.noreply.github.com>
2024-02-05 17:31:31 -0800

    added clockModule and viewHistory

---
+++ app/src/main/java/com/example/optimate/EmployeeFlow/viewHistory.kt
@@ -0,0 +1,12 @@
+package com.example.optimate.EmployeeFlow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.optimate.R
+
+class viewHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_history)
+    }
+}
