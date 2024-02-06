74cbe7277f79ad20e0763282e95a57a4aaaf3723
masamoe <105935014+masamoe@users.noreply.github.com>
2024-02-05 16:24:21 -0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("com.google.ai.client.generativeai:generativeai:0.1.2")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
