0b2f376fe7bad3dcec8e79b9be7ef6a6fbcab84f
Rangathan <akreyken@gmail.com>
2024-02-12 11:40:44 -0800

    modules

--- app/src/main/java/com/example/optimate/loginAndRegister/ModuleChoosingMain.kt
+++ app/src/main/java/com/example/optimate/loginAndRegister/ModuleChoosingMain.kt
@@ -15,10 +15,52 @@ class ModuleChoosingMain : AppCompatActivity() {
         val container1 = findViewById<TextView>(R.id.container1)
         val container2 = findViewById<TextView>(R.id.container2)
         val container3 = findViewById<TextView>(R.id.container3)
+        var container1Picked = false
+        var container2Picked = false
+        var container3Picked = false
+        val module1Price = 9.99
+        val module2Price = 9.99
+        val module3Price = 9.99
+        var amount = findViewById<TextView>(R.id.Amount)
+        val one = "one"
+        val two = "two"
+        val three = "three"
+        
 
-        container1.setOnClickListener { changeColor(container1) }
-        container2.setOnClickListener { changeColor(container2) }
-        container3.setOnClickListener { changeColor(container3) }
+
+        container1.setOnClickListener {
+            changeColor(container1)
+            if (container1Picked) {
+                amount.text = (amount.text.toString().toDouble() - module1Price).toString()
+            } else {
+                amount.text = (amount.text.toString().toDouble() + module1Price).toString()
+            }
+            container1Picked = !container1Picked
+        }
+
+        container2.setOnClickListener {
+            changeColor(container2)
+            if (container2Picked) {
+                amount.text = (amount.text.toString().toDouble() - module2Price).toString()
+            } else {
+                amount.text = (amount.text.toString().toDouble() + module2Price).toString()
+            }
+            container2Picked = !container2Picked
+        }
+
+        container3.setOnClickListener {
+            changeColor(container3)
+            if (container3Picked) {
+                amount.text = (amount.text.toString().toDouble() - module3Price).toString()
+            } else {
+                amount.text = (amount.text.toString().toDouble() + module3Price).toString()
+            }
+            container3Picked = !container3Picked
+        }
+
+
+
+
     }
 
     private fun changeColor(view: View) {
