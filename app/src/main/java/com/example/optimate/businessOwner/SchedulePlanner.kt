2522f844b22563226da384a9afcfdf0706c2cfd1
masamoe <105935014+masamoe@users.noreply.github.com>
2024-02-20 18:08:20 -0800

    Update SchedulePlanner.kt

--- app/src/main/java/com/example/optimate/businessOwner/SchedulePlanner.kt
+++ app/src/main/java/com/example/optimate/businessOwner/SchedulePlanner.kt
@@ -1,5 +1,6 @@
 package com.example.optimate.businessOwner
 
+import android.content.Intent
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,9 +22,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
@@ -43,31 +50,36 @@ fun SchedulePlanner() {
     Scaffold(
         topBar = { XmlTopBar(titleText = "Scheduler") },
         content = { innerPadding ->
-            HorizontalPager(
-                state = pagerState,
-                count = shiftsData.size,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-            ) { page ->
-                Column(modifier = Modifier.padding(innerPadding)) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        horizontalArrangement = Arrangement.SpaceEvenly
+            Column(){
+                HorizontalPager(
+                    state = pagerState,
+                    count = shiftsData.size,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                ) { page ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            horizontalArrangement = Arrangement.SpaceEvenly
 
-                    ) {
-                        ShiftDate(shift = shiftsData[page])
-                    }
-                    Row(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp, vertical = 8.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween) {
+                        ) {
+                            ShiftDate(shift = shiftsData[page])
+                        }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 8.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
                             ShiftsPage(shift = shiftsData[page])
                         }
-                    }
 
+                    }
+                    AddShift(modifier = Modifier.padding(16.dp))
+                }
             }
         }
     )
@@ -132,6 +144,23 @@ fun ShiftCard(shift: Shift) {
                 Text(text = "- $employee")
             }
         }
+    }
+}
+
+@Composable
+fun AddShift(modifier: Modifier = Modifier){
+    val addShiftBtn = LocalContext.current
+    val buttonColor = MaterialTheme.colors.run { Color(0xFF75f8e2) }
+    Button(
+        onClick = {
+            val intent = Intent(addShiftBtn, AddShiftActivity::class.java)
+            addShiftBtn.startActivity(intent)
+        },
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(buttonColor),
+        elevation = ButtonDefaults.buttonElevation(defaultElevation = 8.dp, pressedElevation = 16.dp)
+    ) {
+        Text("Add Shift", color = Color.Black, fontWeight = FontWeight.SemiBold, fontSize = 12.sp)
     }
 }
 
