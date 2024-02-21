07f32a4355d48202ef36fc8018ed13b3077735ba
hisonlin <hisonlinca@gmail.com>
2024-02-20 22:50:46 -0800

    fixed ClockModule

--- app/src/main/java/com/example/optimate/employeeFlow/ClockModule.kt
+++ app/src/main/java/com/example/optimate/employeeFlow/ClockModule.kt
@@ -12,33 +12,22 @@ import com.example.optimate.R
 import java.text.SimpleDateFormat
 import java.util.*
 import android.content.SharedPreferences
+import android.util.Log
+import com.example.optimate.loginAndRegister.GlobalUserData
+import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
-
-
+import com.google.firebase.firestore.firestore
+import java.time.format.DateTimeFormatter
 class ClockModule : AppCompatActivity() {
     private lateinit var digitalClock: TextView
     private lateinit var clockInButton: Button
     private lateinit var clockOutButton: Button
     private var isInClockInState = true
+    private var isOnBreak = false // Added state to track break status
     private val handler = Handler(Looper.getMainLooper())
     private lateinit var sharedPreferences: SharedPreferences
-    // Initialize Firestore
-    private val db = FirebaseFirestore.getInstance()
-    private var currentWorkLog: WorkLog? = null
-
-    // Function to save WorkLog object to Firestore
-    private fun saveWorkLogToFirestore(workLog: WorkLog) {
-        // Add the workLog object to Firestore
-        db.collection("workLogs")
-            .add(workLog)
-            .addOnSuccessListener { documentReference ->
-                println("DocumentSnapshot added with ID: ${documentReference.id}")
-                currentWorkLog = workLog // Update currentWorkLog after saving to Firestore
-            }
-            .addOnFailureListener { e ->
-                println("Error adding document: $e")
-            }
-    }
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,22 +37,12 @@ class ClockModule : AppCompatActivity() {
         clockOutButton = findViewById(R.id.clockOut)
         sharedPreferences = getSharedPreferences("ClockingState", Context.MODE_PRIVATE)
 
-        // Load the clocking state
+        // Load the clocking and break states
         isInClockInState = sharedPreferences.getBoolean("isInClockInState", true)
-        // Check if there's an existing work log
-        // If yes, load it and update UI accordingly
-        // Here, you need to implement a method to fetch the work log from Firestore based on your application logic
-        // I'll assume there's a method called fetchWorkLogFromFirestore() for this purpose
-        fetchWorkLogFromFirestore()
+        isOnBreak = sharedPreferences.getBoolean("isOnBreak", false)
 
-        // Update UI based on loaded clocking state
-        if (!isInClockInState) {
-            // If not in clock in state, set clock out UI
-            clockInButton.text = getString(R.string.clock_out)
-            clockInButton.backgroundTintList = getColorStateList(R.color.light_red)
-            clockOutButton.text = getString(R.string.start_break)
-            clockOutButton.backgroundTintList = getColorStateList(R.color.light_yellow)
-        }
+        // Update UI based on loaded states
+        updateButtonStates()
 
         // Initialize the handler for the digital clock updates
         handler.post(object : Runnable {
@@ -81,144 +60,131 @@ class ClockModule : AppCompatActivity() {
         clockOutButton.setOnClickListener {
             toggleClockState(clockOutButton)
         }
-
-        val viewHistoryButton: Button = findViewById(R.id.viewHistory)
-
-       /* viewHistoryButton.setOnClickListener {
-            val intent = Intent(this, ViewHistory::class.java)
-            startActivity(intent)
-        }*/
     }
 
-    /*private fun fetchWorkLogFromFirestore(userId: String, businessId: String) {
-        // Construct the Firestore query to retrieve the work log document for the current user in the specified business
-        db.collection("workLogs")
-            .whereEqualTo("uid", userId) // Assuming 'uid' is the field that stores user ID
-            .whereEqualTo("bid", businessId) // Assuming 'bid' is the field that stores business ID
-            .get()
-            .addOnSuccessListener { documents ->
-                // Check if any document is returned
-                if (!documents.isEmpty) {
-                    // Get the first document (assuming there's only one work log per user in a business)
-                    val documentSnapshot = documents.documents[0]
-                    // Parse the data and update the currentWorkLog variable
-                    currentWorkLog = documentSnapshot.toObject(WorkLog::class.java)
-                    // You may also want to update the UI here based on the retrieved work log
-                } else {
-                    // No work log found for the user in the business, handle this case if needed
-                }
-            }
-            .addOnFailureListener { e ->
-                // Handle failure to fetch work log
-                println("Error fetching work log: $e")
-            }
-    }*/
-
-    private fun fetchWorkLogFromFirestore() {
-        // Get the current user's ID, replace this with your actual method to get the user ID
-        val userId = "asnckjsancjksankcas"
-        // Get the current business's ID, replace this with your actual method to get the business ID
-        val businessId = "kasjnckjsanjcksan"
-
-        // Construct the Firestore query to retrieve the work log document for the current user and business
-        db.collection("workLogs")
-            .whereEqualTo("UID", userId)
-            .whereEqualTo("BID", businessId)
-            .get()
-            .addOnSuccessListener { documents ->
-                // Check if any document is returned
-                if (!documents.isEmpty) {
-                    // Get the first document (assuming there's only one work log per user for a business)
-                    val documentSnapshot = documents.documents[0]
-                    // Parse the data and update the currentWorkLog variable
-                    currentWorkLog = documentSnapshot.toObject(WorkLog::class.java)
-                    // You may also want to update the UI here based on the retrieved work log
-                } else {
-                    // No work log found for the user and business, handle this case if needed
-                }
-            }
-            .addOnFailureListener { e ->
-                // Handle failure to fetch work log
-                println("Error fetching work log: $e")
-            }
-    }
-    private fun toggleClockState(clickedButton: Button) {
-
-
-        // Toggle the clock state
-        isInClockInState = !isInClockInState
-
-        // Save the clocking state
-        sharedPreferences.edit().putBoolean("isInClockInState", isInClockInState).apply()
-
-        // Get current date and time
-        val currentDateAndTime = Calendar.getInstance().time
-
-        when {
-            clickedButton == clockInButton && clockInButton.text == getString(R.string.clock_in) -> {
-                // Clock In state
-                clockInButton.text = getString(R.string.start_break)
-                clockInButton.backgroundTintList = getColorStateList(R.color.light_yellow)
-                clockOutButton.backgroundTintList = getColorStateList(R.color.light_red)
-                clockOutButton.setTextColor(getColor(R.color.black))
-                clockOutButton.isEnabled = true
-
-                // Save clock in time to work log
-                currentWorkLog?.clockIn = getCurrentTime()
-            }
-            clickedButton == clockInButton && clockInButton.text == getString(R.string.clock_out) -> {
-                // Clock In state
-                clockInButton.text = getString(R.string.clock_in)
-                clockInButton.backgroundTintList = getColorStateList(R.color.light_green)
-                clockOutButton.text = getString(R.string.clock_out)
-                clockOutButton.isEnabled = false
-                clockOutButton.backgroundTintList = getColorStateList(R.color.light_grey)
-                clockOutButton.setTextColor(getColor(R.color.grey))
-
-                // Save the work log to Firestore
-                currentWorkLog?.clockOut = getCurrentTime()
-                currentWorkLog?.let { saveWorkLogToFirestore(it) }
-
-            }
-            clickedButton == clockInButton && clockInButton.text == getString(R.string.start_break) -> {
-                // Start Break state
-                clockInButton.text = getString(R.string.end_break)
-                clockInButton.backgroundTintList = getColorStateList(R.color.light_yellow)
-            }
-            clickedButton == clockInButton && clockInButton.text == getString(R.string.end_break) -> {
-                // End Break state
-                clockInButton.text = getString(R.string.clock_out)
-                clockInButton.backgroundTintList = getColorStateList(R.color.light_red)
-                clockOutButton.text = getString(R.string.start_break)
-                clockOutButton.backgroundTintList = getColorStateList(R.color.light_yellow)
-
-            }
-            clickedButton == clockOutButton && clockOutButton.text == getString(R.string.start_break) -> {
-                // Start Break state for Clock Out button
-                clockInButton.text = getString(R.string.end_break)
-                clockOutButton.text = getString(R.string.clock_out)
-                clockInButton.backgroundTintList = getColorStateList(R.color.light_yellow)
-                clockOutButton.backgroundTintList = getColorStateList(R.color.light_red)
-            }
-            clickedButton == clockOutButton && clockOutButton.text == getString(R.string.clock_out) -> {
-                // Clock Out state
-                clockOutButton.isEnabled = false
-                clockInButton.text = getString(R.string.clock_in)
-                clockInButton.backgroundTintList = getColorStateList(R.color.light_green)
-                clockOutButton.text = getString(R.string.clock_out)
-                clockOutButton.backgroundTintList = getColorStateList(R.color.light_grey)
-                clockOutButton.setTextColor(getColor(R.color.grey))
-
-                // Save the work log to Firestore
-                currentWorkLog?.clockOut = getCurrentTime()
-                currentWorkLog?.let { saveWorkLogToFirestore(it) }
-
-
-            }
+    private fun updateButtonStates() {
+        if (isOnBreak) {
+            // UI for break state
+            clockInButton.text = getString(R.string.end_break)
+            clockInButton.backgroundTintList = getColorStateList(R.color.light_yellow)
+            clockOutButton.isEnabled = false
+        } else if (!isInClockInState) {
+            // UI for clocked out state
+            clockInButton.text = getString(R.string.clock_in)
+            clockInButton.backgroundTintList = getColorStateList(R.color.light_green)
+            clockOutButton.text = getString(R.string.clock_out)
+            clockOutButton.isEnabled = false
+            clockOutButton.backgroundTintList = getColorStateList(R.color.light_grey)
+            clockOutButton.setTextColor(getColor(R.color.grey))
+        } else {
+            // UI for clocked in state
+            clockInButton.text = getString(R.string.clock_out)
+            clockInButton.backgroundTintList = getColorStateList(R.color.light_red)
+            clockOutButton.text = getString(R.string.start_break)
+            clockOutButton.isEnabled = true
+            clockOutButton.backgroundTintList = getColorStateList(R.color.light_yellow)
         }
     }
 
+    private fun toggleClockState(clickedButton: Button) {
+        when {
+            clickedButton == clockInButton && clockInButton.text == getString(R.string.clock_in) -> {
+                // Clock In state
+                isInClockInState = true
+                isOnBreak = false // Ensure not on break when clocking in
+                saveWorkLogToDB("clockIn")
+            }
+            clickedButton == clockInButton && clockInButton.text == getString(R.string.clock_out) -> {
+                // Clock Out state
+                isInClockInState = false
+                isOnBreak = false // Ensure not on break when clocking out
+                saveWorkLogToDB("clockOut")
+            }
+            clickedButton == clockInButton && clockInButton.text == getString(R.string.start_break) -> {
+                // Start Break state
+                isOnBreak = true
+                saveWorkLogToDB("breakStart")
+            }
+            clickedButton == clockInButton && clockInButton.text == getString(R.string.end_break) -> {
+                // End Break state
+                isOnBreak = false
+                saveWorkLogToDB("breakEnd")
+            }
+            clickedButton == clockOutButton && clockOutButton.text == getString(R.string.start_break) -> {
+                // Start Break state for Clock Out button
+                isOnBreak = true
+                saveWorkLogToDB("breakStart")
+            }
+        }
 
+        // Save the clocking and break states
+        sharedPreferences.edit().apply {
+            putBoolean("isInClockInState", isInClockInState)
+            putBoolean("isOnBreak", isOnBreak)
+            apply()
+        }
+
+        // After toggling, update the button states
+        updateButtonStates()
+    }
+
+    private fun saveWorkLogToDB(type: String) {
+        val bid = GlobalUserData.bid
+        val uid = GlobalUserData.uid
+        // Format the current Date and Time into a readable string
+        val dateTimeFormat = SimpleDateFormat("MM/dd/yyyy HH:mm:ss", Locale.getDefault())
+        val formattedDateTime = dateTimeFormat.format(Date())
+
+        val entry = hashMapOf(
+            type to formattedDateTime
+        )
+
+        // Get the current date
+        val currentDate = SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(Date())
+
+        db.collection("workLogs")
+            .whereEqualTo("uid", uid)
+            .whereEqualTo("bid", bid)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents.isEmpty) {
+                    val workLog = hashMapOf(
+                        "uid" to uid,
+                        "bid" to bid,
+                        currentDate to listOf(entry)
+                    )
+                    db.collection("workLogs")
+                        .add(workLog)
+                        .addOnSuccessListener { documentReference ->
+                            Log.d("WorkLog", "DocumentSnapshot added with ID: ${documentReference.id}")
+                        }
+                        .addOnFailureListener { e ->
+                            Log.w("WorkLog", "Error adding document", e)
+                        }
+                } else {
+                    val documentSnapshot = documents.documents[0]
+                    val existingData = documentSnapshot.get(currentDate)
+
+                    if (existingData == null) {
+                        // currentDate key does not exist, create a new list for currentDate
+                        documentSnapshot.reference.update(currentDate, listOf(entry))
+                    } else {
+                        // currentDate key exists, append to its list
+                        val clocks: MutableList<HashMap<String, String>> = if (existingData is List<*>) {
+                            existingData.mapNotNull { it as? HashMap<String, String> }.toMutableList()
+                        } else {
+                            mutableListOf()
+                        }
+
+                        clocks.add(entry)
+                        documentSnapshot.reference.update(currentDate, clocks)
+                    }
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w("WorkLog", "Error getting documents.", e)
+            }
+    }
 
     private fun updateDigitalClock() {
         // Get the current time
@@ -232,14 +198,12 @@ class ClockModule : AppCompatActivity() {
         digitalClock.text = formattedTime
     }
 
-    private fun getCurrentTime(): String {
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-        return dateFormat.format(Date())
-    }
-
     override fun onDestroy() {
         // Remove the callbacks to prevent memory leaks
         handler.removeCallbacksAndMessages(null)
         super.onDestroy()
     }
+
 }
+
+
