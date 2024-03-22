2fa6529feec182855c7f7a8732ab443befc57162
Rangathan <akreyken@gmail.com>
2024-03-22 16:48:46 -0700

    Update AccountsActivity.kt

--- app/src/main/java/com/example/optimate/businessOwner/AccountsActivity.kt
+++ app/src/main/java/com/example/optimate/businessOwner/AccountsActivity.kt
@@ -43,16 +43,17 @@ class AccountsActivity : AppCompatActivity() {
     private fun fetchAccounts(bid: String) {
         db.collection("users")
             .whereEqualTo("BID", bid)
-            .whereNotEqualTo("role", "businessOwner")
             .whereIn("account_status.status", listOf("Created", "Active")) // Modified line
             .get()
             .addOnSuccessListener { documents ->
                 val fetchedAccounts = mutableListOf<Account>()
                 for (document in documents) {
-                    val name = document.getString("name") ?: "N/A"
-                    val title = document.getString("title") ?: "N/A"
-                    val uid = document.getString("UID") ?: "N/A"
-                    fetchedAccounts.add(Account(name, title, uid))
+                    if(document.getString("role") != "businessOwner") {
+                        val name = document.getString("name") ?: "N/A"
+                        val title = document.getString("title") ?: "N/A"
+                        val uid = document.getString("UID") ?: "N/A"
+                        fetchedAccounts.add(Account(name, title, uid))
+                    }
                 }
 
                 accountsList.clear()
